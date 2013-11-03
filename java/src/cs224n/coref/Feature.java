1129aceb6eaac20580f109db80650613cb9fa815
Xincheng Zhang <xinczhan@gmail.com>
2013-11-03 15:26:27 -0800

    Classified based first commit MUC 0.682 B1 0.686

--- java/src/cs224n/coref/Feature.java
+++ java/src/cs224n/coref/Feature.java
@@ -9,100 +9,243 @@ import java.util.Set;
  */
 public interface Feature {
 
-  //-----------------------------------------------------------
-  // TEMPLATE FEATURE TEMPLATES
-  //-----------------------------------------------------------
-  public static class PairFeature implements Feature {
-    public final Pair<Feature,Feature> content;
-    public PairFeature(Feature a, Feature b){ this.content = Pair.make(a, b); }
-    public String toString(){ return content.toString(); }
-    public boolean equals(Object o){ return o instanceof PairFeature && ((PairFeature) o).content.equals(content); }
-    public int hashCode(){ return content.hashCode(); }
-  }
+	// -----------------------------------------------------------
+	// TEMPLATE FEATURE TEMPLATES
+	// -----------------------------------------------------------
+	public static class PairFeature implements Feature {
+		public final Pair<Feature, Feature> content;
 
-  public static abstract class Indicator implements Feature {
-    public final boolean value;
-    public Indicator(boolean value){ this.value = value; }
-    public boolean equals(Object o){ return o instanceof Indicator && o.getClass().equals(this.getClass()) && ((Indicator) o).value == value; }
-    public int hashCode(){ 
-    	return this.getClass().hashCode() ^ Boolean.valueOf(value).hashCode(); }
-    public String toString(){ 
-    	return this.getClass().getSimpleName() + "(" + value + ")"; }
-  }
+		public PairFeature(Feature a, Feature b) {
+			this.content = Pair.make(a, b);
+		}
 
-  public static abstract class IntIndicator implements Feature {
-    public final int value;
-    public IntIndicator(int value){ this.value = value; }
-    public boolean equals(Object o){ return o instanceof IntIndicator && o.getClass().equals(this.getClass()) && ((IntIndicator) o).value == value; }
-    public int hashCode(){ 
-    	return this.getClass().hashCode() ^ value; 
-    }
-    public String toString(){ return this.getClass().getSimpleName() + "(" + value + ")"; }
-  }
+		public String toString() {
+			return content.toString();
+		}
 
-  public static abstract class BucketIndicator implements Feature {
-    public final int bucket;
-    public final int numBuckets;
-    public BucketIndicator(int value, int max, int numBuckets){
-      this.numBuckets = numBuckets;
-      bucket = value * numBuckets / max;
-      if(bucket < 0 || bucket >= numBuckets){ throw new IllegalStateException("Bucket out of range: " + value + " max="+max+" numbuckets="+numBuckets); }
-    }
-    public boolean equals(Object o){ return o instanceof IntIndicator && o.getClass().equals(this.getClass()) && ((IntIndicator) o).value == bucket; }
-    public int hashCode(){ return this.getClass().hashCode() ^ bucket; }
-    public String toString(){ return this.getClass().getSimpleName() + "(" + bucket + "/" + numBuckets + ")"; }
-  }
+		public boolean equals(Object o) {
+			return o instanceof PairFeature
+					&& ((PairFeature) o).content.equals(content);
+		}
 
-  public static abstract class Placeholder implements Feature {
-    public Placeholder(){ }
-    public boolean equals(Object o){ return o instanceof Placeholder && o.getClass().equals(this.getClass()); }
-    public int hashCode(){ return this.getClass().hashCode(); }
-    public String toString(){ return this.getClass().getSimpleName(); }
-  }
+		public int hashCode() {
+			return content.hashCode();
+		}
+	}
 
-  public static abstract class StringIndicator implements Feature {
-    public final String str;
-    public StringIndicator(String str){ this.str = str; }
-    public boolean equals(Object o){ return o instanceof StringIndicator && o.getClass().equals(this.getClass()) && ((StringIndicator) o).str.equals(this.str); }
-    public int hashCode(){ return this.getClass().hashCode() ^ str.hashCode(); }
-    public String toString(){ return this.getClass().getSimpleName() + "(" + str + ")"; }
-  }
+	public static abstract class Indicator implements Feature {
+		public final boolean value;
 
-  public static abstract class SetIndicator implements Feature {
-    public final Set<String> set;
-    public SetIndicator(Set<String> set){ this.set = set; }
-    public boolean equals(Object o){ return o instanceof SetIndicator && o.getClass().equals(this.getClass()) && ((SetIndicator) o).set.equals(this.set); }
-    public int hashCode(){ return this.getClass().hashCode() ^ set.hashCode(); }
-    public String toString(){
-      StringBuilder b = new StringBuilder();
-      b.append(this.getClass().getSimpleName());
-      b.append("( ");
-      for(String s : set){
-        b.append(s).append(" ");
-      }
-      b.append(")");
-      return b.toString();
-    }
-  }
-  
-  /*
-   * TODO: If necessary, add new feature types
-   */
+		public Indicator(boolean value) {
+			this.value = value;
+		}
 
-  //-----------------------------------------------------------
-  // REAL FEATURE TEMPLATES
-  //-----------------------------------------------------------
+		public boolean equals(Object o) {
+			return o instanceof Indicator
+					&& o.getClass().equals(this.getClass())
+					&& ((Indicator) o).value == value;
+		}
 
-  public static class CoreferentIndicator extends Indicator {
-    public CoreferentIndicator(boolean coreferent){ super(coreferent); }
-  }
+		public int hashCode() {
+			return this.getClass().hashCode()
+					^ Boolean.valueOf(value).hashCode();
+		}
 
-  public static class ExactMatch extends Indicator {
-    public ExactMatch(boolean exactMatch){ super(exactMatch); }
-  }
-  
-  /*
-   * TODO: Add values to the indicators here.
-   */
+		public String toString() {
+			return this.getClass().getSimpleName() + "(" + value + ")";
+		}
+	}
+
+	public static abstract class IntIndicator implements Feature {
+		public final int value;
+
+		public IntIndicator(int value) {
+			this.value = value;
+		}
+
+		public boolean equals(Object o) {
+			return o instanceof IntIndicator
+					&& o.getClass().equals(this.getClass())
+					&& ((IntIndicator) o).value == value;
+		}
+
+		public int hashCode() {
+			return this.getClass().hashCode() ^ value;
+		}
+
+		public String toString() {
+			return this.getClass().getSimpleName() + "(" + value + ")";
+		}
+	}
+
+	public static abstract class BucketIndicator implements Feature {
+		public final int bucket;
+		public final int numBuckets;
+
+		public BucketIndicator(int value, int max, int numBuckets) {
+			this.numBuckets = numBuckets;
+			bucket = value * numBuckets / max;
+			if (bucket < 0 || bucket >= numBuckets) {
+				throw new IllegalStateException("Bucket out of range: " + value
+						+ " max=" + max + " numbuckets=" + numBuckets);
+			}
+		}
+
+		public boolean equals(Object o) {
+			return o instanceof IntIndicator
+					&& o.getClass().equals(this.getClass())
+					&& ((IntIndicator) o).value == bucket;
+		}
+
+		public int hashCode() {
+			return this.getClass().hashCode() ^ bucket;
+		}
+
+		public String toString() {
+			return this.getClass().getSimpleName() + "(" + bucket + "/"
+					+ numBuckets + ")";
+		}
+	}
+
+	public static abstract class Placeholder implements Feature {
+		public Placeholder() {
+		}
+
+		public boolean equals(Object o) {
+			return o instanceof Placeholder
+					&& o.getClass().equals(this.getClass());
+		}
+
+		public int hashCode() {
+			return this.getClass().hashCode();
+		}
+
+		public String toString() {
+			return this.getClass().getSimpleName();
+		}
+	}
+
+	public static abstract class StringIndicator implements Feature {
+		public final String str;
+
+		public StringIndicator(String str) {
+			this.str = str;
+		}
+
+		public boolean equals(Object o) {
+			return o instanceof StringIndicator
+					&& o.getClass().equals(this.getClass())
+					&& ((StringIndicator) o).str.equals(this.str);
+		}
+
+		public int hashCode() {
+			return this.getClass().hashCode() ^ str.hashCode();
+		}
+
+		public String toString() {
+			return this.getClass().getSimpleName() + "(" + str + ")";
+		}
+	}
+
+	public static abstract class SetIndicator implements Feature {
+		public final Set<String> set;
+
+		public SetIndicator(Set<String> set) {
+			this.set = set;
+		}
+
+		public boolean equals(Object o) {
+			return o instanceof SetIndicator
+					&& o.getClass().equals(this.getClass())
+					&& ((SetIndicator) o).set.equals(this.set);
+		}
+
+		public int hashCode() {
+			return this.getClass().hashCode() ^ set.hashCode();
+		}
+
+		public String toString() {
+			StringBuilder b = new StringBuilder();
+			b.append(this.getClass().getSimpleName());
+			b.append("( ");
+			for (String s : set) {
+				b.append(s).append(" ");
+			}
+			b.append(")");
+			return b.toString();
+		}
+	}
+
+	/*
+	 * TODO: If necessary, add new feature types
+	 */
+
+	// -----------------------------------------------------------
+	// REAL FEATURE TEMPLATES
+	// -----------------------------------------------------------
+
+	public static class CoreferentIndicator extends Indicator {
+		public CoreferentIndicator(boolean coreferent) {
+			super(coreferent);
+		}
+	}
+
+	public static class ExactMatch extends Indicator {
+		public ExactMatch(boolean exactMatch) {
+			super(exactMatch);
+		}
+	}
+
+	public static class HeadMatch extends Indicator {
+		public HeadMatch(boolean headMatch) {
+			super(headMatch);
+		}
+	}
+	
+	public static class GenderMatch extends Indicator {
+		public GenderMatch(boolean match) {
+			super(match);
+		}
+	}
+	
+	public static class NumberMatch extends Indicator {
+		public NumberMatch(boolean match) {
+			super(match);
+		}
+	}
+	
+	
+	public static class IsFixedPron extends Indicator {
+		public IsFixedPron(boolean isPron) {
+			super(isPron);
+		}
+	}
+	
+	public static class IsCandidatePron extends Indicator {
+		public IsCandidatePron(boolean isPron) {
+			super(isPron);
+		}
+	}
+
+	public static class SentenceDistance extends IntIndicator {
+		public SentenceDistance(int dist) {
+			super(dist);
+		}
+	}
+
+	public static class MentionDistance extends IntIndicator {
+		public MentionDistance(int dist) {
+			super(dist);
+		}
+	}
+
+	public static class Path extends StringIndicator {
+		public Path(String path) {
+			super(path);
+		}
+	}
+	/*
+	 * TODO: Add values to the indicators here.
+	 */
 
 }
--- java/src/cs224n/corefsystems/ClassifierBased.java
+++ java/src/cs224n/corefsystems/ClassifierBased.java
@@ -1,7 +1,9 @@
 package cs224n.corefsystems;
 
 import cs224n.coref.*;
+import cs224n.coref.Feature.Path;
 import cs224n.util.Pair;
+import cs224n.util.UnorderedPair;
 import edu.stanford.nlp.classify.LinearClassifier;
 import edu.stanford.nlp.classify.LinearClassifierFactory;
 import edu.stanford.nlp.classify.RVFDataset;
@@ -21,193 +23,275 @@ import static edu.stanford.nlp.util.logging.Redwood.Util.*;
  */
 public class ClassifierBased implements CoreferenceSystem {
 
-	private static <E> Set<E> mkSet(E[] array){
+	private static <E> Set<E> mkSet(E[] array) {
 		Set<E> rtn = new HashSet<E>();
 		Collections.addAll(rtn, array);
 		return rtn;
 	}
 
-	private static final Set<Object> ACTIVE_FEATURES = mkSet(new Object[]{
+	private static final Set<Object> ACTIVE_FEATURES = mkSet(new Object[] {
 
-			/*
-			 * TODO: Create a set of active features
-			 */
+	/*
+	 * TODO: Create a set of active features
+	 */
 
-			Feature.ExactMatch.class,
+	Feature.ExactMatch.class,
+			Feature.HeadMatch.class,
+			Feature.GenderMatch.class,
+			Feature.NumberMatch.class,
+			Feature.SentenceDistance.class,
+			Feature.MentionDistance.class,
+			Feature.Path.class,
+			// skeleton for how to create a pair feature
+			Pair.make(Feature.IsCandidatePron.class, Feature.IsFixedPron.class), });
 
-			//skeleton for how to create a pair feature
-			//Pair.make(Feature.IsFeature1.class, Feature.IsFeature2.class),
-	});
+	private LinearClassifier<Boolean, Feature> classifier;
 
-
-	private LinearClassifier<Boolean,Feature> classifier;
-
-	public ClassifierBased(){
+	public ClassifierBased() {
 		StanfordRedwoodConfiguration.setup();
 		RedwoodConfiguration.current().collapseApproximate().apply();
 	}
 
-	public FeatureExtractor<Pair<Mention,ClusteredMention>,Feature,Boolean> extractor = new FeatureExtractor<Pair<Mention, ClusteredMention>, Feature, Boolean>() {
-		private <E> Feature feature(Class<E> clazz, Pair<Mention,ClusteredMention> input, Option<Double> count){
-			
-			//--Variables
-			Mention onPrix = input.getFirst(); //the first mention (referred to as m_i in the handout)
-			Mention candidate = input.getSecond().mention; //the second mention (referred to as m_j in the handout)
-			Entity candidateCluster = input.getSecond().entity; //the cluster containing the second mention
+	public FeatureExtractor<Pair<Mention, ClusteredMention>, Feature, Boolean> extractor = new FeatureExtractor<Pair<Mention, ClusteredMention>, Feature, Boolean>() {
+		private <E> Feature feature(Class<E> clazz,
+				Pair<Mention, ClusteredMention> input, Option<Double> count) {
 
+			// --Variables
+			Mention onPrix = input.getFirst(); // the first mention (referred to
+												// as m_i in the handout)
+			Mention candidate = input.getSecond().mention; // the second mention
+															// (referred to as
+															// m_j in the
+															// handout)
+			Entity candidateCluster = input.getSecond().entity; // the cluster
+																// containing
+																// the second
+																// mention
 
-			//--Features
-			if(clazz.equals(Feature.ExactMatch.class)){
-				//(exact string match)
-				return new Feature.ExactMatch(onPrix.gloss().equals(candidate.gloss()));
-//			} else if(clazz.equals(Feature.NewFeature.class) {
-				/*
-				 * TODO: Add features to return for specific classes. Implement calculating values of features here.
-				 */
-			}
-			else {
-				throw new IllegalArgumentException("Unregistered feature: " + clazz);
+			// --Features
+			if (clazz.equals(Feature.ExactMatch.class)) {
+				// (exact string match)
+				return new Feature.ExactMatch(onPrix.gloss().equals(
+						candidate.gloss()));
+			} else if (clazz.equals(Feature.HeadMatch.class)) {
+				return new Feature.HeadMatch(onPrix.headWord().equals(
+						candidate.headWord()));
+			} else if (clazz.equals(Feature.GenderMatch.class)) {
+				return new Feature.GenderMatch(Util.haveGenderAndAreSameGender(
+						onPrix, candidateCluster) == Pair.make(true, true));
+			} else if (clazz.equals(Feature.NumberMatch.class)) {
+				return new Feature.NumberMatch(Util.haveNumberAndAreSameNumber(
+						onPrix, candidateCluster) == Pair.make(true, true));
+			} else if (clazz.equals(Feature.SentenceDistance.class)) {
+				return new Feature.SentenceDistance(Math.abs(onPrix.doc
+						.indexOfSentence(onPrix.sentence)
+						- candidate.doc.indexOfSentence(candidate.sentence)));
+			} else if (clazz.equals(Feature.MentionDistance.class)) {
+				return new Feature.MentionDistance(Math.abs(onPrix.doc
+						.indexOfMention(onPrix)
+						- candidate.doc.indexOfMention(candidate)));
+			} else if (clazz.equals(Feature.IsCandidatePron.class)) {
+				return new Feature.IsCandidatePron(
+						Pronoun.isSomePronoun(candidate.gloss()));
+			} else if (clazz.equals(Feature.Path.class)) {
+				if (onPrix.sentence == candidate.sentence
+						&& onPrix.headWordIndex != candidate.headWordIndex) {
+					Sentence sentence = onPrix.sentence;
+					String path = "";
+					Iterable<Pair<String, Integer>> paths = sentence.parse
+							.getTraversalBetween(onPrix.headWordIndex,
+									candidate.headWordIndex);
+					if (paths == null)
+						return new Feature.Path(path);
+					for (Pair<String, Integer> pair : paths) {
+						path += "/" + pair.getFirst();
+					}
+					return new Feature.Path(path);
+				}
+				return new Feature.Path("No Path");
+			} else if (clazz.equals(Feature.IsFixedPron.class)) {
+				return new Feature.IsFixedPron(Pronoun.isSomePronoun(onPrix
+						.gloss()));
+			} else {
+				throw new IllegalArgumentException("Unregistered feature: "
+						+ clazz);
 			}
 		}
 
-		@SuppressWarnings({"unchecked"})
+		@SuppressWarnings({ "unchecked" })
 		@Override
-		protected void fillFeatures(Pair<Mention, ClusteredMention> input, Counter<Feature> inFeatures, Boolean output, Counter<Feature> outFeatures) {
-			//--Input Features
-			for(Object o : ACTIVE_FEATURES){
-				if(o instanceof Class){
-					//(case: singleton feature)
+		protected void fillFeatures(Pair<Mention, ClusteredMention> input,
+				Counter<Feature> inFeatures, Boolean output,
+				Counter<Feature> outFeatures) {
+			// --Input Features
+			for (Object o : ACTIVE_FEATURES) {
+				if (o instanceof Class) {
+					// (case: singleton feature)
 					Option<Double> count = new Option<Double>(1.0);
 					Feature feat = feature((Class) o, input, count);
-					if(count.get() > 0.0){
+					if (count.get() > 0.0) {
 						inFeatures.incrementCount(feat, count.get());
 					}
-				} else if(o instanceof Pair){
-					//(case: pair of features)
-					Pair<Class,Class> pair = (Pair<Class,Class>) o;
+				} else if (o instanceof Pair) {
+					// (case: pair of features)
+					Pair<Class, Class> pair = (Pair<Class, Class>) o;
 					Option<Double> countA = new Option<Double>(1.0);
 					Option<Double> countB = new Option<Double>(1.0);
 					Feature featA = feature(pair.getFirst(), input, countA);
 					Feature featB = feature(pair.getSecond(), input, countB);
-					if(countA.get() * countB.get() > 0.0){
-						inFeatures.incrementCount(new Feature.PairFeature(featA, featB), countA.get() * countB.get());
+					if (countA.get() * countB.get() > 0.0) {
+						inFeatures.incrementCount(new Feature.PairFeature(
+								featA, featB), countA.get() * countB.get());
 					}
 				}
 			}
 
-			//--Output Features
-			if(output != null){
-				outFeatures.incrementCount(new Feature.CoreferentIndicator(output), 1.0);
+			// --Output Features
+			if (output != null) {
+				outFeatures.incrementCount(new Feature.CoreferentIndicator(
+						output), 1.0);
 			}
 		}
 
 		@Override
 		protected Feature concat(Feature a, Feature b) {
-			return new Feature.PairFeature(a,b);
+			return new Feature.PairFeature(a, b);
 		}
 	};
 
 	public void train(Collection<Pair<Document, List<Entity>>> trainingData) {
 		startTrack("Training");
-		//--Variables
+		// --Variables
 		RVFDataset<Boolean, Feature> dataset = new RVFDataset<Boolean, Feature>();
-		LinearClassifierFactory<Boolean, Feature> fact = new LinearClassifierFactory<Boolean,Feature>();
-		//--Feature Extraction
+		LinearClassifierFactory<Boolean, Feature> fact = new LinearClassifierFactory<Boolean, Feature>();
+		// --Feature Extraction
 		startTrack("Feature Extraction");
-		for(Pair<Document,List<Entity>> datum : trainingData){
-			//(document variables)
+		for (Pair<Document, List<Entity>> datum : trainingData) {
+			// (document variables)
 			Document doc = datum.getFirst();
 			List<Entity> goldClusters = datum.getSecond();
 			List<Mention> mentions = doc.getMentions();
-			Map<Mention,Entity> goldEntities = Entity.mentionToEntityMap(goldClusters);
+			Map<Mention, Entity> goldEntities = Entity
+					.mentionToEntityMap(goldClusters);
 			startTrack("Document " + doc.id);
-			//(for each mention...)
-			for(int i=0; i<mentions.size(); i++){
-				//(get the mention and its cluster)
+			// (for each mention...)
+			for (int i = 0; i < mentions.size(); i++) {
+				// (get the mention and its cluster)
 				Mention onPrix = mentions.get(i);
 				Entity source = goldEntities.get(onPrix);
-				if(source == null){ throw new IllegalArgumentException("Mention has no gold entity: " + onPrix); }
-				//(for each previous mention...)
+				if (source == null) {
+					throw new IllegalArgumentException(
+							"Mention has no gold entity: " + onPrix);
+				}
+				// (for each previous mention...)
 				int oldSize = dataset.size();
-				for(int j=i-1; j>=0; j--){
-					//(get previous mention and its cluster)
+				for (int j = i - 1; j >= 0; j--) {
+					// (get previous mention and its cluster)
 					Mention cand = mentions.get(j);
 					Entity target = goldEntities.get(cand);
-					if(target == null){ throw new IllegalArgumentException("Mention has no gold entity: " + cand); }
-					//(extract features)
-					Counter<Feature> feats = extractor.extractFeatures(Pair.make(onPrix, cand.markCoreferent(target)));
-					//(add datum)
-					dataset.add(new RVFDatum<Boolean, Feature>(feats, target == source));
-					//(stop if
-					if(target == source){ break; }
+					if (target == null) {
+						throw new IllegalArgumentException(
+								"Mention has no gold entity: " + cand);
+					}
+					// (extract features)
+					Counter<Feature> feats = extractor.extractFeatures(Pair
+							.make(onPrix, cand.markCoreferent(target)));
+					// (add datum)
+					dataset.add(new RVFDatum<Boolean, Feature>(feats,
+							target == source));
+					// (stop if
+					if (target == source) {
+						break;
+					}
 				}
-				//logf("Mention %s (%d datums)", onPrix.toString(), dataset.size() - oldSize);
+				// logf("Mention %s (%d datums)", onPrix.toString(),
+				// dataset.size() - oldSize);
 			}
 			endTrack("Document " + doc.id);
 		}
 		endTrack("Feature Extraction");
-		//--Train Classifier
+		// --Train Classifier
 		startTrack("Minimizer");
 		this.classifier = fact.trainClassifier(dataset);
 		endTrack("Minimizer");
-		//--Dump Weights
+		// --Dump Weights
 		startTrack("Features");
-		//(get labels to print)
+		// (get labels to print)
 		Set<Boolean> labels = new HashSet<Boolean>();
 		labels.add(true);
-		//(print features)
-		for(Triple<Feature,Boolean,Double> featureInfo : this.classifier.getTopFeatures(labels, 0.0, true, 100, true)){
+		// (print features)
+		for (Triple<Feature, Boolean, Double> featureInfo : this.classifier
+				.getTopFeatures(labels, 0.0, true, 100, true)) {
 			Feature feature = featureInfo.first();
 			Boolean label = featureInfo.second();
 			Double magnitude = featureInfo.third();
-			//log(FORCE,new DecimalFormat("0.000").format(magnitude) + " [" + label + "] " + feature);
+			// log(FORCE,new DecimalFormat("0.000").format(magnitude) + " [" +
+			// label + "] " + feature);
 		}
 		end_Track("Features");
 		endTrack("Training");
 	}
 
 	public List<ClusteredMention> runCoreference(Document doc) {
-		//--Overhead
+		// --Overhead
 		startTrack("Testing " + doc.id);
-		//(variables)
-		List<ClusteredMention> rtn = new ArrayList<ClusteredMention>(doc.getMentions().size());
+		// (variables)
+		List<ClusteredMention> rtn = new ArrayList<ClusteredMention>(doc
+				.getMentions().size());
 		List<Mention> mentions = doc.getMentions();
 		int singletons = 0;
-		//--Run Classifier
-		for(int i=0; i<mentions.size(); i++){
-			//(variables)
+		// --Run Classifier
+		for (int i = 0; i < mentions.size(); i++) {
+			// (variables)
 			Mention onPrix = mentions.get(i);
 			int coreferentWith = -1;
-			//(get mention it is coreferent with)
-			for(int j=i-1; j>=0; j--){
+			// (get mention it is coreferent with)
+			for (int j = i - 1; j >= 0; j--) {
 				ClusteredMention cand = rtn.get(j);
-				boolean coreferent = classifier.classOf(new RVFDatum<Boolean, Feature>(extractor.extractFeatures(Pair.make(onPrix, cand))));
-				if(coreferent){
+				boolean coreferent = classifier
+						.classOf(new RVFDatum<Boolean, Feature>(extractor
+								.extractFeatures(Pair.make(onPrix, cand))));
+				if (coreferent) {
 					coreferentWith = j;
 					break;
 				}
 			}
-			//(mark coreference)
-			if(coreferentWith < 0){
+			// (mark coreference)
+			if (coreferentWith < 0) {
 				singletons += 1;
 				rtn.add(onPrix.markSingleton());
 			} else {
-				//log("Mention " + onPrix + " coreferent with " + mentions.get(coreferentWith));
+				// log("Mention " + onPrix + " coreferent with " +
+				// mentions.get(coreferentWith));
 				rtn.add(onPrix.markCoreferent(rtn.get(coreferentWith)));
 			}
 		}
-		//log("" + singletons + " singletons");
-		//--Return
+		// log("" + singletons + " singletons");
+		// --Return
 		endTrack("Testing " + doc.id);
 		return rtn;
 	}
 
 	private class Option<T> {
 		private T obj;
-		public Option(T obj){ this.obj = obj; }
-		public Option(){};
-		public T get(){ return obj; }
-		public void set(T obj){ this.obj = obj; }
-		public boolean exists(){ return obj != null; }
+
+		public Option(T obj) {
+			this.obj = obj;
+		}
+
+		public Option() {
+		};
+
+		public T get() {
+			return obj;
+		}
+
+		public void set(T obj) {
+			this.obj = obj;
+		}
+
+		public boolean exists() {
+			return obj != null;
+		}
 	}
 }
